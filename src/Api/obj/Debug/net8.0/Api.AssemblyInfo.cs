cb7f4f5ca331087fc616e3e98fb19a86d8703fa6
SebeK2221 <144722562+SebeK2221@users.noreply.github.com>
2024-07-03 13:08:26 +0200

    add new migration

--- src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13cfba1b3befd669933cf2b9621865ef9f816885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa1961049aba3450f39779b68de506bc97f454b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13cfba1b3befd669933cf2b9621865ef9f816885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa1961049aba3450f39779b68de506bc97f454b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
